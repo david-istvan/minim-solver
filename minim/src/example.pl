b931e213c2694351cfce3232cd0c3a87f4ed6734
Istvan David <goerenh@gmail.com>
2013-11-17 17:25:02 +0100

    :(

--- minim/src/example.pl
+++ minim/src/example.pl
@@ -11,3 +11,19 @@ tester :-
         Y in 2..3,
         X#>=Y,
         labeling([min], [X,Y]).
+
+list_sum([Item], Item).
+
+list_sum([Item1,Item2 | Tail], Total) :-
+    SumItem is Item1+Item2,
+    list_sum([SumItem|Tail], Total).
+
+deleteSome(L, _, L).
+deleteSome(L, D, R) :-
+  select(E, L, L1),
+  select(E, D, D1),
+  !, deleteSome(L1, D1, R).
+
+select(X, [X|Tail], Tail).
+select(Elem, [Head|Tail], [Head|Rest]) :-
+select(Elem, Tail, Rest).
--- minim/src/minim.pl
+++ minim/src/minim.pl
@@ -2,16 +2,39 @@
 
 /*ugraphs, wgraphs*/
 
-edge(1,2).
-edge(1,3).
-edge(2,3).
-edge(2,4).
-edge(3,4).
+:- use_module(library(chr)).
+:- use_module(library(lists)).
 
-weight(1,1).
-weight(2,1).
-weight(3,2).
-weight(4,3).
+handler leq.
+handler merge.
+constraints leq/2.
+constraints merge/4.
+constraints minimize/1.
+% X leq Y means variable X is less-or-equal to variable Y
+:- op(500, xfx, leq).
+
+reflexivity @ X leq Y <=> X = Y | true.
+antisymmetry @ X leq Y , Y leq X <=> X=Y.
+idempotence @ X leq Y \ X leq Y <=> true.
+transitivity @ X leq Y , Y leq Z ==> X leq Z.
+
+merge @ merge(X, W1, Y, W2) <=> connected(X,Y), W1 is W2 | true.
+
+%minimize @ minimize(WL) <=> mergeweight(_, _, WL, NWL) | NWL.
+minimize @ minimize(WL) <=> removeelement(_, _, WL, NWL) | NWL.
+
+mergeweight(X, Y, WL, NWL):-
+        select([X,W], WL, R1),
+        select([Y,W], R1, R2),
+        W2 is W+1,
+        append(R2, [[X, W2]], NWL);
+        mergeweight(_, _, NWL, NWL).
+
+removeelement(X, W, WL, NWL):-
+        select([X,W], WL, R),
+        W2 is W+1,
+        append(R, [[X, W2]], NWL),
+        removeelement(_, _, NWL, NWL).
 
 connected(X,Y) :-
         edge(X,Y);
@@ -24,4 +47,3 @@ sameWeight(X,Y) :-
 mergeable(X,Y) :-
         connected(X,Y),
         sameWeight(X,Y).
-
