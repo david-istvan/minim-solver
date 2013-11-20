2d7548b549c5af38f8ba0b7bbdf8b145f46cb6ed
Istvan David <goerenh@gmail.com>
2013-11-20 19:19:17 +0100

    handled connectivity

--- minim/src/minim.pl
+++ minim/src/minim.pl
@@ -6,29 +6,53 @@
 :- use_module(library(lists)).
 
 handler minimize.
-constraints minimize/1.
+constraints minimize/2.
 
-%minimize @ minimize(WL) <=> mergeweight(_, _, WL, NWL) | NWL.
-minimize @ minimize(WL) <=> mergeWeightList(WL) | WL.
-
-hasMergeableElements(WL):-
-        select([_X,W], WL, R1),
-        select([_Y,W], R1, _).
+minimize @ minimize(WL, EL) <=> mergeWeightList(WL, EL) | true.
 
 notMinimized(WL):-
         length(WL, L),
         L>1.
 
-mergeWeightList(WL):-
-        writeln(WL),
-        notMinimized(WL),
-        hasMergeableElements(WL),
+isConnected(X, Y, EL):-
+        select([X,Y], EL, _).
+
+mergeNodes(X, Y, EL, ID, NEL):-
+        writeln('current edge list: ' + EL),
+        (
+           select([X,Z1], EL, R1), Z1\=Y ->
+                append(R1, [[ID, Z1]], R1),
+                select([X, Y], R1, NEL),
+                writeln('new edge list: '+NEL);
+           select([Z2,X], EL, R2), Z2\=Y ->
+                append(R2, [[Z2, ID]], R1),
+                select([X, Y], R1, NEL),
+                writeln('new edge list: '+NEL);
+           select([Y,Z3], EL, R3), Z3\=X ->
+                append(R3, [[ID, Z3]], R1),
+                select([X, Y], R1, NEL),
+                writeln('new edge list: '+NEL);
+           select([Z4,Y], EL, R4), Z4\=X ->                
+                append(R4, [[Z4, ID]], R1),
+                select([X, Y], R1, NEL),
+                writeln('new edge list: '+NEL);              
+           select([X, Y], EL, NEL) ->
+                writeln('new edge list: '+NEL);
+           select([Y, X], EL, NEL) ->
+                writeln('new edge list: '+NEL)).
+
+mergeWeightList(WL, EL):-
+        writeln('current weight list: ' + WL),
         select([X,W], WL, R1),
-        select([_Y,W], R1, R2),
+        select([Y,W], R1, R2),
+        %isConnected(X, Y, EL),
+        X<Y,
         W2 is W+1,
-        X2 is X+10,
-        append(R2, [[X2, W2]], NWL),
-        mergeWeightList(NWL).
+        ID is X+10,
+        mergeNodes(X, Y, EL, ID, NEL),
+        append(R2, [[ID, W2]], NWL),
+        writeln('new weight list: ' + NWL),
+        mergeWeightList(NWL, NEL).
 
 writeln(Str) :-
         write(Str), nl.
