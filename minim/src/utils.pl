2dc9b5777a9f8917302448767c9dc62ce3f6c79a
Istvan David <goerenh@gmail.com>
2013-11-24 15:45:48 +0100

    using built-in max_member now

--- minim/src/utils.pl
+++ minim/src/utils.pl
@@ -1,22 +1,12 @@
 :-  module(utils, [next_id/2]).
 
+:- use_module(library(lists)).
+
 next_id(WL, NextId):-
         ids(WL, IDList),
-        maxlist(IDList, MaxId),
+        max_member(MaxId, IDList),
         NextId is MaxId+1.
 
 ids([], []).
 ids([A-_B|Pairs], [A|Keys]) :-
     ids(Pairs, Keys).
-
-max(X,Y,Max) :-
-        X > Y,!,
-        Max = X.
-max(_X,Y,Max) :-
-        Max = Y.
-
-maxlist([],0).
-maxlist([X],X).
-maxlist([X,Y|List],Max) :-
-        max(X,Y,Max1),
-        maxlist([Max1|List],Max).
