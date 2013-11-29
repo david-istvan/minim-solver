7347b5e9e5c08dc2e4a5f43f73c15fe94b4e3425
Istvan David <goerenh@gmail.com>
2013-11-29 14:40:16 +0100

    improved Minim API

--- minim.tester/src/minim/tester/Tester.java
+++ minim.tester/src/minim/tester/Tester.java
@@ -1,14 +1,16 @@
 package minim.tester;
 
+import minim.tester.Minim.Graph;
 import se.sics.jasper.SPException;
 
 public class Tester {
 
 	public static void main(String[] args) {
 		try {
-			MinimAPI minimAPI = new MinimAPI(args);
-			minimAPI.setGraphData(TestData.getWeights(), TestData.getEdges());
-			minimAPI.execute();
+			Minim minim = new Minim();
+			Graph graph = minim.newGraph().withWeights(TestData.getWeights()).withEdges(TestData.getEdges());
+			int minimalSolution = minim.findMinimalSolution(graph);
+			System.out.println("Minimal solution found: " + minimalSolution + ".");
 		} catch (SPException e) {
 			e.printStackTrace();
 		}
