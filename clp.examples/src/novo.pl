6e5afd4809b3f1723ed3f88b1e8d37b8d9182499
Istvan David <goerenh@gmail.com>
2013-11-30 22:47:07 +0100

    added solution for task #3

--- clp.examples/src/novo.pl
+++ clp.examples/src/novo.pl
@@ -10,3 +10,23 @@ novo(X, [Y|Ls], N):-
         .
 
 novo(_, [], _).
+
+szeszam([X,Y,Z|Tail], K):-
+        makeSzeszam(X, Y, Z, K, K1),
+        (
+           length(Tail,Lt), Lt>0
+        ->
+           szeszam([Y,Z|Tail], K1)
+        ;
+           K1 #= 0
+        )
+        .
+ 
+szeszam([], _).
+
+makeSzeszam(X, Y, Z, K0, K1):-
+        write('testing '), write(X), write(Y), write(Z), nl,
+        (((X#<Y) #/\ (Y#>Z)) #\/ ((X#>Y) #/\ (Y#<Z))) #<=> S,
+        S #=1,
+        K1 is K0-S
+        .
